8b20e08ed15ad29342823dee244cc8adee3d1f4a
christiangnez <103572319+christiangnez@users.noreply.github.com>
2022-04-13 11:42:56 +0000

    Add files via upload

---
+++ drivetest.py
@@ -0,0 +1,20 @@
+class Voiture(object):
+    def __init__(self, marque, couleur):
+        self.marque= marque
+        self.couleur= couleur
+        self.conducteur='personne'
+        self.vitesse= 0
+    def choisirConducteur(self, nom):
+        self.conducteur= nom
+    def affich_info(self, marque, couleur, conducteur, vitesse):
+        print(self, marque, couleur, conducteur, vitesse)
+
+maVoiture= Voiture('Ford','rouge')
+taVoiture= Voiture('toyota','bleue')
+saVoiture= Voiture('Ford','noir')
+
+maVoiture.choisirConducteur('hassan')
+taVoiture.choisirConducteur('afid')
+saVoiture.choisirConducteur('ali')
+
+
